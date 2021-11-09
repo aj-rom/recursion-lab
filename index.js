c81710c8362d6c6ae38c3f993e926d564bd064e6
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 18:43:04 -0700

    Completed.

--- index.js
+++ index.js
@@ -10,24 +10,35 @@ function reverseString(str) {
     return str.length < 2 ? str : reverseString(str.slice(1)) + str[0]
 }
 
-function maxOf() {
+function maxOf(arr) {
+    if (arr.length < 2)
+        return arr[0]
 
+    if (arr[1] < arr[0])
+        arr[1] = arr[0]
+
+    return maxOf(arr.slice(1))
 }
 
-function includesNumber() {
+function includesNumber(arr, num) {
+    if (arr.length === 1)
+        return false
 
+    if (arr[0] === num)
+        return true
+
+    return includesNumber(arr.slice(1), num)
+}
+
+function addUpTo(arr, idx) {
+    if (idx < 1) return arr[0]
+
+    arr[1] = arr[0] + arr[1]
+    return addUpTo(arr.slice(1), idx - 1)
 }
 
 function isPalindrome(str) {
     let len = str.length
-
-    // Is palindrome if we reach this
-    if (len < 2)
-        return true
-    // If first and last index are equal continue
-    else if (str[0] === str[len - 1])
-        return isPalindrome(str.slice(1), len - 1)
-    // Is not a palindrome
-    else
-        return false
+    if (len <= 2) return true
+    return str[0] !== str[len - 1] ? false : isPalindrome(str.slice(1, len - 1))
 }
